e0fbbaf6b6b791c2cb3727186d230598ac108378
Shivek Khurana <khuranashivek@gmail.com>
2012-11-19 23:29:53 +0530

    Api added

---
+++ api.py
@@ -0,0 +1,8 @@
+from affine import *
+
+while(True):
+	a = raw_input("enter text or file name to operate on : ")
+	l = raw_input("enter file_name of script to operate : ")
+
+	
+	if raw_input("do you want to continue y/n : ") == 'n' : break
